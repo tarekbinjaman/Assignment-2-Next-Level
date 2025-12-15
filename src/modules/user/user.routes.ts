810990f0f163e37f6f23c90cdbd80455236017fb
tarekbinjaman <tarekbinjaman@gmail.com>
2025-12-15 17:20:21 +0600

    routes added

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { userControllers } from "./user.controller";
 
 const router = Router();
 
-router.post("/", )
+router.post("/", userControllers.createUser);
+router.get("/", userControllers.getUser);
+router.get("/:id", userControllers.getSingleUser);
+router.put("/:id", userControllers.updateUser);
+router.delete("/:id", userControllers.deleteUser);
+
+export const userRoutes = router;
