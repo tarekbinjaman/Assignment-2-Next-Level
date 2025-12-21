d2b6dc0e9623f2b7f6577e2016c0fd840227c979
tarekbinjaman <tarekbinjaman@gmail.com>
2025-12-21 19:10:01 +0600

    booking responce from admin side done

--- src/modules/booking/booking.service.ts
+++ src/modules/booking/booking.service.ts
@@ -23,31 +23,58 @@ const createBooking = async (Payload: Record<string, unknown>) => {
 
   // price calculation
   const total_Price = numberOfDays * daily_rent_price;
-  const status = "active"
-  const bookngResult = await pool.query(`
+  const status = "active";
+  const bookngResult = await pool.query(
+    `
     INSERT INTO bookings 
     (customer_id, vehicle_id, rent_start_date, rent_end_date, total_Price, status)
     VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING *;
     `,
-  [
-    customer_id,
-    vehicle_id,
-    rent_start_date,
-    rent_end_date,
-    total_Price,
-    status
-  ]
+    [
+      customer_id,
+      vehicle_id,
+      rent_start_date,
+      rent_end_date,
+      total_Price,
+      status,
+    ]
   );
   return {
     ...bookngResult.rows[0],
     vehicle: {
       vehicle_name,
-      daily_rent_price
-    }
-  }
+      daily_rent_price,
+    },
+  };
+};
+
+const getBookingsByAdmin = async () => {
+  const result = await pool.query(`
+    SELECT
+    b.id
+    b.customer_id,
+    b.vehicle_id,
+    b.rent_start_date,
+    b.total_price,
+    b.tatus,
+
+    u.name As customer_name,
+    u.eamil as customer_email,,
+
+    v.vehicle_name,
+    v.registration_number
+
+    FROM bookings b
+    JOIN users u ON b.customer_id = u.id
+    JOIN vehicles v ON b.vehicle_id = v.id
+    ORDER BY b.created_at DESC;
+    `);
+
+  return result.rows;
 };
 
 export const bookingService = {
   createBooking,
+  getBookingsByAdmin
 };
